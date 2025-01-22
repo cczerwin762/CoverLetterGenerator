5dcd80d9807e86d2e291e60bc34dc2106d3a5503
Chris Czerwinski <cczerwinski123@gmail.com>
2025-01-21 19:15:12 -0700

    adding additonal functionality and todo's

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import os
 import openai
 
+#GLOBALS
+cstrCaptionSumRes = "Please summarize my resume: " 
+cstrCaptionRes = "The following is a summary of my resume: "
+cstrCaptionRole = "Could you please write me a professional coverletter for the following job description: "
 def main():
     strRoleDescription = input("Please copy and paste the description for your role from the job board: ")
-    blnResumeExists = False
+    
     strGPTAPIKey = ""
+    if os.path.isfile("./api_keys/apiKey.txt"):
+        with open("./api_keys/apiKey.txt","r") as f:
+            strGPTAPIKey = f.read()
+    else: 
+        strGPTAPIKey = input("No api key for ChatGpt detected. Please copy and paste your api key here: ")
+        # TODO: put in error catching in case the api key doesn't work
+        with open("./api_keys/apiKey.txt","w+") as f:
+            f.writelines(strGPTAPIKey)
+    openai.api_key = strGPTAPIKey
+
+    blnResumeExists = False
     strResume = ""
-    if (os.path.isfile("./resume/resume.txt")):
+    if os.path.isfile("./resume/resume.txt"):
         blnResumeExists = True
     if not blnResumeExists:
         strResume = input("No resume texted. Please copy and paste a text file of your resume: ")
@@ -14,10 +29,11 @@ def main():
         strReplaceResume = input("Would you like to continue with your current resume or replace it (1 - replace. any other entry will keep ur current resume): ")
         if strReplaceResume == "1": 
             strResume = input("Please copy and paste a text file of your resume: ")
-            # delete resume
-    openai.api_key = strGPTAPIKey
+            # TODO: delete resume
+    # TODO: encapsulate this following section into a function summarizeResume()
     if strResume != "":
-        message = [{"role": "user", "content": ("Please summarize my resume: " + strResume)}]
+        message = [{"role": "user", "content": (cstrCaptionSumRes + strResume)}]
+        # TODO: Make extra text a global variable
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=message
         )
@@ -27,5 +43,14 @@ def main():
     else: 
         with open("./resume/resume.txt", "r") as f:
             strResume = f.read()
+
+    message = [{"role": "user", "content": (cstrCaptionRes + strResume + cstrCaptionRole + strRoleDescription)}]
+    chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=message
+        )
+    coverLetter = chat.choices[0].message.content
+    print(coverLetter)
+    # TODO: save as txt file or pdf. 
+
 if __name__ =="__main__":
     main()
